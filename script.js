1c2de86d0f45adecb24c761a93fc8ec1508bf20c
leehyogeun <dkdkdk99835@gmail.com>
2021-12-16 00:14:16 +0900

    control fps

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ class Symbol {
     this.text = this.characters.charAt(
       Math.floor(Math.random() * this.characters.length)
     );
-    context.fillStyle = "#0aff0a";
     context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
     if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
       this.y = 0;
@@ -47,12 +46,33 @@ class Effect {
 }
 
 const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 15;
+const nextFrame = 1000 / fps; //트리거하고 그릴 때 까지 기다리는 밀리초의 양
+// 다음 초는 초당 30 프레임을 원하므로 nextFrame만큼 공백이 있어야함
+let timer = 0; // 델타타임 누적변수
+// deltaTime ins the difference in milliseconds between the
+// previous animation frame and the current animation frame
 
-function animate() {
-  ctx.fillStyle = "rgba(0,0,0,0.05)"; // 투명한 레이어가 덧씌워지면서 위에게 투명해짐
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.font = effect.fontSize + "px monospace"; // monospace 동일한 양의 수평 공간 차지함
-  effect.symbols.forEach((symbol) => symbol.draw(ctx));
-  requestAnimationFrame(animate);
+function animate(timeStamp) {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  if (timer > nextFrame) {
+    ctx.fillStyle = "rgba(0,0,0,0.05)"; // 투명한 레이어가 덧씌워지면서 위에게 투명해짐
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.textAlign = "center";
+    ctx.fillStyle = "#0aff0a";
+    ctx.font = effect.fontSize + "px monospace"; // monospace 동일한 양의 수평 공간 차지함
+    effect.symbols.forEach((symbol) => symbol.draw(ctx));
+    timer = 0;
+    // 보통은 모니터 주사율에 맞추어 실행
+    // 스택에 연산이 쌓여있으면 한번에 확 이동할 수 도 있음
+  } else {
+    timer += deltaTime;
+  }
+  requestAnimationFrame(animate); // 기본적으로 1초에 60번,
+  console.log(`timeStamp: ${timeStamp}`);
+  console.log(`deltaTime: ${deltaTime}`);
+  console.log(`timer: ${timer}`);
 }
-animate();
+animate(0); // 원하는 timeStamp 값을 넣어줘야 함
