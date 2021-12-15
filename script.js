66d43d36d26c71567145accb405cc70986811abe
leehyogeun <dkdkdk99835@gmail.com>
2021-12-15 17:51:31 +0900

    animation Loop

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ class Symbol {
   }
   draw(context) {
     this.text = this.characters.charAt(
-      Math.floor(Math.random()) * this.characters.length
+      Math.floor(Math.random() * this.characters.length)
     );
     context.fillStyle = "#0aff0a";
-    context.fillText(this.text, this.x, this.fontSize, this.y, this.fontSize);
-    if (this.y * this.fontSize > this.canvasHeight) {
+    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
       this.y = 0;
     } else {
       this.y += 1;
@@ -35,14 +35,24 @@ class Effect {
     this.fontSize = 25;
     this.columns = this.canvasWidth / this.fontSize;
     this.symbols = [];
+    this.#initialize();
   }
   #initialize() {
     // private(은닉)프리픽스 이름 자체의 일부기 떄문에
     // 생성자에서 접근할 때 #을 붙이고 접근해야함
-    for (let i = 0; i < this.columns.length; i++) {
-      this.symbols[i] = new Symbols();
+    for (let i = 0; i < this.columns; i++) {
+      this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
     }
   }
 }
 
-function animate() {}
+const effect = new Effect(canvas.width, canvas.height);
+
+function animate() {
+  ctx.fillStyle = "rgba(0,0,0,0.05)"; // 투명한 레이어가 덧씌워지면서 위에게 투명해짐
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.font = effect.fontSize + "px monospace"; // monospace 동일한 양의 수평 공간 차지함
+  effect.symbols.forEach((symbol) => symbol.draw(ctx));
+  requestAnimationFrame(animate);
+}
+animate();
